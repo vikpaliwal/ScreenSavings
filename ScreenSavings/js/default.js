6e0d910fd653dc153b7eb1e5f3c6fce231fa9c27
unknown <shossai2@SHOSSAI2-MOBL.amr.corp.intel.com>
2012-06-29 14:37:48 -0700

    'init'

--- ScreenSavings/js/default.js
+++ ScreenSavings/js/default.js
@@ -34,7 +34,7 @@
             while (hascur) {
                 var cur = iter.current.value;
                 if (cur.name === taskName) {
-                    cur.unregister(true);                    
+                    cur.unregister(true);
                 }
                 hascur = iter.moveNext();
             }
@@ -43,16 +43,32 @@
     };
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
+    var userId = null;
+    var channelUri = null;
+    var channelExpiration = null;
+
     WinJS.strictProcessing();
 
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                BackgroundTask.unregisterBackgroundTasks(BackgroundTask.BackgroundTaskName);
-                BackgroundTask.registerBackgroundTask(BackgroundTask.BackgroundTaskEntryPoint,
-                                                    BackgroundTask.BackgroundTaskName,
-                                                    new Windows.ApplicationModel.Background.SystemTrigger(Windows.ApplicationModel.Background.SystemTriggerType.timeZoneChange, false),
-                                                    null);
+
+                WL.init({
+                    scope: ["wl.signin", "wl.basic"]
+                });
+                WL.login().then(
+                    function (response) {
+                        if (response.status == 'connected') {
+                            getUserId();
+                        }
+                        else {
+                            loginFailed(response.status);
+                        }
+                    },
+                    function (responseFailed) {
+                        loginFailed(responseFailed.error);
+                    });
+
             } else {
                 // TODO: This application has been reactivated from suspension.
                 // Restore application state here.
@@ -60,6 +76,78 @@
             args.setPromise(WinJS.UI.processAll());
         }
     };
+
+    var getUserId = function () {
+        WL.api({
+            path: "me",
+            method: "GET"
+        }).then(
+            function (response) {
+                userId = response.id;
+                openNotificationChannel();
+            },
+            function (responseFailed) {
+                loginFailed(responseFailed.error);
+            }
+        );
+    };
+
+    var openNotificationChannel = function () {
+        if (userId == null) {
+            loginFailed('user not cunnected');
+            return;
+        }
+
+        Windows.ApplicationModel.Background.BackgroundExecutionManager
+            .requestAccessAsync().then(function (backgroundStatus) {
+                if (backgroundStatus != Windows.ApplicationModel.Background.BackgroundAccessStatus.denied
+                        && backgroundStatus != Windows.ApplicationModel.Background.BackgroundAccessStatus.unspecified) {
+
+                    Windows.Networking.PushNotifications.PushNotificationChannelManager
+                        .createPushNotificationChannelForApplicationAsync().then(
+                            function (channelOperation) {
+                                registerNotificationChannel(channelOperation.uri, channelOperation.expirationTime);
+                            });
+                } else {
+                    //user did not allow lock screen access. hence no push notification.
+                }
+            });
+    };
+
+    var registerBackgroundTask = function () {
+        BackgroundTask.unregisterBackgroundTasks(BackgroundTask.BackgroundTaskName);
+        BackgroundTask.registerBackgroundTask(BackgroundTask.BackgroundTaskEntryPoint,
+                                                BackgroundTask.BackgroundTaskName,
+                                                new Windows.ApplicationModel.Background.PushNotificationTrigger(),
+                                                null);
+    };
+
+    var registerNotificationChannel = function (newChannel, newExpiration) {
+        if (newChannel != channelUri) {
+            registerBackgroundTask();
+
+            WCFClientRuntimeComponent.WCFServiceCaller.registerNotificationChannelAsync(userId, newChannel, newExpiration).then(
+                function (result) {
+                    if (result == 'Success') {
+                        channelUri = newChannel;
+                        channelExpiration = newExpiration;
+                        $('#dump').text("UserId:" + userId
+                            + "\n\nChannel:" + channelUri);
+                    }
+
+                });
+        }
+    };
+
+    var loginFailed = function (errorMessage) {
+        $('#dump').text(errorMessage);
+        //probably exit from app with an error.
+    }
+
+    var resuming = function () {
+        openNotificationChannel();
+    };
+
 
     app.oncheckpoint = function (args) {
         // TODO: This application is about to be suspended. Save any state
@@ -70,5 +158,7 @@
         // args.setPromise().
     };
 
+    Windows.UI.WebUI.WebUIApplication.addEventListener("resuming", resuming, false);
     app.start();
+
 })();
---
+++ WCFClientRuntimeComponent/WCFServiceCaller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFClientRuntimeComponent.ScreenSavingsServiceReference;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+
+namespace WCFClientRuntimeComponent
+{
+    public sealed class WCFServiceCaller
+    {
+        public static string GetAnswer()
+        {
+            return "The answer is 42.";
+        }
+
+        public static IAsyncOperation<string> GetDataAsync(int input)
+        {            
+            return AsyncInfo.Run<string>((token) => 
+                Task.Run<string>(() =>
+                {
+                    PushNotificationServiceClient client = new PushNotificationServiceClient();
+                    var task = client.GetDataAsync( input );
+                    client.CloseAsync();
+                    return task;
+                }, token
+                )
+            );  
+        }
+
+        public static IAsyncOperation<string> RegisterNotificationChannelAsync(string userId, string channelUri, string channelExpiration)
+        {
+            return AsyncInfo.Run<string>((token) =>
+                Task.Run<string>(() =>
+                {
+                    PushNotificationServiceClient client = new PushNotificationServiceClient();
+                    var task = client.RegisterNotificationChannelAsync(userId, channelUri, channelExpiration);
+                    client.CloseAsync();
+                    return task;
+                }, token
+                )
+            );
+        }
+
+    }
+}
